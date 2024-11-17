2a58cb94a5f5414e96b38325ac68d934b63ee1c3
SharpJype <jyjool10@gmail.com>
2024-11-17 18:55:16 +0200

    update

--- word.py
+++ word.py
@@ -10,13 +10,18 @@ def loop(lan="en"):
             if lan=="fi": word = textprc.fantasyfinnish(textprc.randomword(l+1, lan))
             else: word = textprc.randomword(l+1, lan)
             print(f" {total}.", word.title())
-        x = input()
+        x = input("more? [Y/n]")
+        if x.lower()=="n": break
+
+def main():
+    while 1:
+        print("options: en/fi/fr/es/eo/hu/nl/da/de/is/se/pt/pl/it/cs/la/ja/jah/jak")
+        lan = input("language: ").lower()
+        try: loop(lan)
+        except KeyError:
+            print("invalid language")
+        print("")
 
 if __name__ == "__main__":
-    print("language options:")
-    print("\ten/fi/fr/es/eo/hu/nl/da/de/is/se/pt/pl/it/cs/la/ja/jah/jak")
-    while 1:
-        lan = input("choose language: ")
-        try: loop(lan)
-        except KeyError: print("invalid language")
+    main()
 
